9189b8ad7275e1a306d154c9ce3b1b0a68c86fe2
ViktorOster <Ozterkvlt@hotmail.com>
2019-04-05 17:37:27 +0300

    still fixing static files....

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,10 @@ EMAIL_HOST_USER = 'Auctions@gmail.com'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join('static'), )
 # STATICFILES_DIRS = (os.path.join('static'), )
